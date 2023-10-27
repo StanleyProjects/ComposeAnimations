347f0fd0d2ff238dcbbf9813a943cb7d21282bc8
Stanley Wintergreen <kepocnhh@gmail.com>
2023-10-27 14:43:40 +0300

    fade

--- app/src/main/kotlin/sp/sample/animations/MainActivity.kt
+++ app/src/main/kotlin/sp/sample/animations/MainActivity.kt
@@ -3,21 +3,62 @@ package sp.sample.animations
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.text.BasicText
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import sp.ax.jc.animations.AnimatedFadeVisibility
+import kotlin.time.Duration.Companion.seconds
 
 internal class MainActivity : AppCompatActivity() {
     override fun onCreate(inState: Bundle?) {
         super.onCreate(inState)
         setContent {
-            Box(Modifier.fillMaxSize()) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.White)
+                    .padding(vertical = 48.dp),
+            ) {
+                val visibleState = remember { mutableStateOf(false) }
                 BasicText(
-                    modifier = Modifier.align(Alignment.Center),
-                    text = BuildConfig.APPLICATION_ID,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(64.dp)
+                        .align(Alignment.BottomCenter)
+                        .clickable {
+                            visibleState.value = !visibleState.value
+                        }
+                        .wrapContentSize(),
+                    text = if (visibleState.value) "hide" else "show",
                 )
+                AnimatedFadeVisibility(
+                    visible = visibleState.value,
+                    modifier = Modifier.align(Alignment.Center),
+                    duration = 0.5.seconds,
+                ) {
+                    BasicText(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(64.dp)
+                            .background(Color.Gray)
+                            .wrapContentSize(),
+                        text = BuildConfig.APPLICATION_ID,
+                    )
+                }
             }
         }
     }
---
+++ lib/src/main/kotlin/sp/ax/jc/animations/Foo.kt
@@ -0,0 +1,49 @@
+package sp.ax.jc.animations
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.AnimatedVisibilityScope
+import androidx.compose.animation.core.FiniteAnimationSpec
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import kotlin.time.Duration
+
+@Composable
+fun AnimatedFadeVisibility(
+    visible: Boolean,
+    modifier: Modifier = Modifier,
+    label: String = "AnimatedVisibility:fade",
+    animationSpec: FiniteAnimationSpec<Float>,
+    outAnimationSpec: FiniteAnimationSpec<Float> = animationSpec,
+    content: @Composable AnimatedVisibilityScope.() -> Unit
+) {
+    AnimatedVisibility(
+        visible = visible,
+        modifier = modifier,
+        label = label,
+        enter = fadeIn(animationSpec),
+        exit = fadeOut(outAnimationSpec),
+        content = content,
+    )
+}
+
+@Composable
+fun AnimatedFadeVisibility(
+    visible: Boolean,
+    modifier: Modifier = Modifier,
+    label: String = "AnimatedVisibility:fade:tween",
+    duration: Duration,
+    outDuration: Duration = duration,
+    content: @Composable AnimatedVisibilityScope.() -> Unit
+) {
+    AnimatedFadeVisibility(
+        visible = visible,
+        modifier = modifier,
+        label = label,
+        animationSpec = tween(duration.inWholeMilliseconds.toInt()),
+        outAnimationSpec = tween(outDuration.inWholeMilliseconds.toInt()),
+        content = content,
+    )
+}
