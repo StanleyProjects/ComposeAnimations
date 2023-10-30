2ded6c9bd834d6156587b5c40d85dff298921afa
Stanley Wintergreen <kepocnhh@gmail.com>
2023-10-30 22:28:16 +0300

    test tween slide

---
+++ lib/src/test/kotlin/sp/ax/jc/animations/tween/slide/AnimatedTest.kt
@@ -0,0 +1,7 @@
+package sp.ax.jc.animations.tween.slide
+
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+internal class AnimatedTest {}
