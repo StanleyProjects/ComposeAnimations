8c4baf54fc61244fe6797f6ef5f1fc7db93c7e5c
Stanley Wintergreen <kepocnhh@gmail.com>
2023-11-02 21:31:06 +0300

    SlideHVisibility style test

--- lib/src/test/kotlin/sp/ax/jc/animations/tween/slide/SlideHVisibilityTest.kt
+++ lib/src/test/kotlin/sp/ax/jc/animations/tween/slide/SlideHVisibilityTest.kt
@@ -1,6 +1,8 @@
 package sp.ax.jc.animations.tween.slide
 
 import androidx.compose.animation.core.AnimationConstants
+import androidx.compose.animation.core.Ease
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.test.assertIsDisplayed
@@ -10,8 +12,11 @@ import androidx.compose.ui.test.performClick
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
+import sp.ax.jc.animations.style.LocalTweenStyle
+import sp.ax.jc.animations.style.TweenStyle
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.Duration.Companion.seconds
 
 @RunWith(RobolectricTestRunner::class)
 internal class SlideHVisibilityTest : AnimatedTest() {
@@ -82,6 +87,83 @@ internal class SlideHVisibilityTest : AnimatedTest() {
         }
         rule.assertAnimation(
             containerTag = animatedContainer,
+            contentTag = animatedContent,
+            performStartToFinish = {
+                rule.onNodeWithTag(switcher).performClick()
+            },
+            performFinishToStart = {
+                rule.onNodeWithTag(switcher).performClick()
+            },
+            inDuration = duration,
+            inDelay = delay,
+            outDuration = duration,
+            outDelay = delay,
+            onContentReady = {
+                rule.onNodeWithTag(animatedContent).assertTextEquals(animatedContent)
+            },
+        )
+    }
+
+    @Test
+    fun styleTest() {
+        val animatedContent = "animatedContent"
+        val switcher = "switcher"
+        val duration = .6.seconds
+        val delay = .3.seconds
+        val easing = Ease
+        rule.setContent {
+            Content(switcherTag = switcher) { visible: Boolean ->
+                SlideHVisibility(
+                    visible = visible,
+                    duration = duration,
+                    delay = delay,
+                    easing = easing,
+                ) {
+                    AnimatedContent(testTag = animatedContent)
+                }
+            }
+        }
+        rule.assertAnimation(
+            contentTag = animatedContent,
+            performStartToFinish = {
+                rule.onNodeWithTag(switcher).performClick()
+            },
+            performFinishToStart = {
+                rule.onNodeWithTag(switcher).performClick()
+            },
+            inDuration = duration,
+            inDelay = delay,
+            outDuration = duration,
+            outDelay = delay,
+            onContentReady = {
+                rule.onNodeWithTag(animatedContent).assertTextEquals(animatedContent)
+            },
+        )
+    }
+
+    @Test
+    fun styleCompositionLocalTest() {
+        val animatedContent = "animatedContent"
+        val switcher = "switcher"
+        val duration = 2.seconds
+        val delay = 1.seconds
+        val easing = Ease
+        rule.setContent {
+            CompositionLocalProvider(
+                LocalTweenStyle provides TweenStyle(
+                    duration = duration,
+                    delay = delay,
+                    easing = easing,
+                )
+            ) {
+                Content(switcherTag = switcher) { visible: Boolean ->
+                    SlideHVisibility(visible = visible) {
+                        AnimatedContent(testTag = animatedContent)
+                    }
+                }
+            }
+        }
+        rule.assertAnimation(
             contentTag = animatedContent,
             performStartToFinish = {
                 rule.onNodeWithTag(switcher).performClick()
