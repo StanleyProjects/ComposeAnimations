a75b411c931b37c86968b567bbdee2f27a8f1418
Stanley Wintergreen <kepocnhh@gmail.com>
2023-11-02 21:27:16 +0300

    SlideHVisibilityTest

--- lib/src/test/kotlin/sp/ax/jc/animations/tween/slide/SlideHVisibilityTest.kt
+++ lib/src/test/kotlin/sp/ax/jc/animations/tween/slide/SlideHVisibilityTest.kt
@@ -1,5 +1,8 @@
 package sp.ax.jc.animations.tween.slide
 
+import androidx.compose.animation.core.AnimationConstants
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.onNodeWithTag
@@ -7,6 +10,8 @@ import androidx.compose.ui.test.performClick
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.milliseconds
 
 @RunWith(RobolectricTestRunner::class)
 internal class SlideHVisibilityTest : AnimatedTest() {
@@ -25,5 +30,72 @@ internal class SlideHVisibilityTest : AnimatedTest() {
         rule.onNodeWithTag(animatedContent).assertIsDisplayed()
         rule.onNodeWithTag(animatedContent).assertTextEquals(animatedContent)
         rule.onNodeWithTag(switcher).performClick()
+    }
+
+    @Test
+    fun defaultTest() {
+        val animatedContent = "animatedContent"
+        val switcher = "switcher"
+        val duration = AnimationConstants.DefaultDurationMillis.milliseconds
+        val delay = Duration.ZERO
+        rule.setContent {
+            Content(switcherTag = switcher) { visible: Boolean ->
+                SlideHVisibility(visible = visible) {
+                    AnimatedContent(testTag = animatedContent)
+                }
+            }
+        }
+        rule.assertAnimation(
+            contentTag = animatedContent,
+            performStartToFinish = {
+                rule.onNodeWithTag(switcher).performClick()
+            },
+            performFinishToStart = {
+                rule.onNodeWithTag(switcher).performClick()
+            },
+            inDuration = duration,
+            inDelay = delay,
+            outDuration = duration,
+            outDelay = delay,
+            onContentReady = {
+                rule.onNodeWithTag(animatedContent).assertTextEquals(animatedContent)
+            },
+        )
+    }
+
+    @Test
+    fun modifierTest() {
+        val animatedContainer = "animatedContainer"
+        val animatedContent = "animatedContent"
+        val switcher = "switcher"
+        val duration = AnimationConstants.DefaultDurationMillis.milliseconds
+        val delay = Duration.ZERO
+        rule.setContent {
+            Content(switcherTag = switcher) { visible: Boolean ->
+                SlideHVisibility(
+                    visible = visible,
+                    modifier = Modifier.testTag(animatedContainer),
+                ) {
+                    AnimatedContent(testTag = animatedContent)
+                }
+            }
+        }
+        rule.assertAnimation(
+            containerTag = animatedContainer,
+            contentTag = animatedContent,
+            performStartToFinish = {
+                rule.onNodeWithTag(switcher).performClick()
+            },
+            performFinishToStart = {
+                rule.onNodeWithTag(switcher).performClick()
+            },
+            inDuration = duration,
+            inDelay = delay,
+            outDuration = duration,
+            outDelay = delay,
+            onContentReady = {
+                rule.onNodeWithTag(animatedContent).assertTextEquals(animatedContent)
+            },
+        )
     }
 }
