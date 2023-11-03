5b7c7830261905e7ea7e92a4ebaa253c1f74000b
Stanley Wintergreen <kepocnhh@gmail.com>
2023-11-03 16:02:31 +0300

    fix doc TweenStyle

--- lib/src/main/kotlin/sp/ax/jc/animations/style/TweenStyle.kt
+++ lib/src/main/kotlin/sp/ax/jc/animations/style/TweenStyle.kt
@@ -8,6 +8,14 @@ import androidx.compose.runtime.staticCompositionLocalOf
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.milliseconds
 
+/**
+ * Stores values to describe how to animate with [TweenSpec][androidx.compose.animation.core.TweenSpec].
+ * @param duration duration of the animation spec
+ * @param delay the amount of time that animation waits before starting
+ * @param easing the easing curve that will be used to interpolate between start and end
+ * @author [Stanley Wintergreen](https://github.com/kepocnhh)
+ * @since 0.0.1
+ */
 @Immutable
 data class TweenStyle(
     val duration: Duration,
@@ -15,6 +23,28 @@ data class TweenStyle(
     val easing: Easing,
 )
 
+/**
+ * Provides [TweenStyle] to describe how to animate with [TweenSpec][androidx.compose.animation.core.TweenSpec].
+ *
+ * Usage:
+ * ```
+ * val visibleState = remember { mutableStateOf(false) }
+ * ...
+ * CompositionLocalProvider(
+ *     LocalTweenStyle provides TweenStyle(
+ *         duration = 1.seconds,
+ *         delay = Duration.ZERO,
+ *         easing = LinearEasing,
+ *     ),
+ * ) {
+ *     SlideHVisibility(visible = visibleState.value) {
+ *         BasicText(text = "foo")
+ *     }
+ * }
+ * ```
+ * @author [Stanley Wintergreen](https://github.com/kepocnhh)
+ * @since 0.0.1
+ */
 val LocalTweenStyle = staticCompositionLocalOf {
     TweenStyle(
         duration = AnimationConstants.DefaultDurationMillis.milliseconds,
