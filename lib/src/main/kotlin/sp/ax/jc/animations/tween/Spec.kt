852be2000e3622135316c56cca04ad8bb963a4f0
Stanley Wintergreen <kepocnhh@gmail.com>
2023-11-03 15:26:23 +0300

    fix doc spec

--- lib/src/main/kotlin/sp/ax/jc/animations/tween/Spec.kt
+++ lib/src/main/kotlin/sp/ax/jc/animations/tween/Spec.kt
@@ -5,6 +5,15 @@ import androidx.compose.animation.core.TweenSpec
 import androidx.compose.animation.core.tween
 import kotlin.time.Duration
 
+/**
+ * Creates a [TweenSpec] configured with the given duration, delay and easing curve.
+ *
+ * @param duration duration of the animation spec
+ * @param delay the amount of time that animation waits before starting
+ * @param easing the easing curve that will be used to interpolate between start and end
+ * @author [Stanley Wintergreen](https://github.com/kepocnhh)
+ * @since 0.0.1
+ */
 fun <T> tween(
     duration: Duration,
     delay: Duration,
--- lib/src/main/kotlin/sp/ax/jc/animations/tween/slide/Transition.kt
+++ lib/src/main/kotlin/sp/ax/jc/animations/tween/slide/Transition.kt
@@ -10,6 +10,41 @@ import androidx.compose.ui.unit.IntSize
 import sp.ax.jc.animations.tween.tween
 import kotlin.time.Duration
 
+/**
+ * The wrapper for the [slideIn][androidx.compose.animation.slideIn] with an `animationSpec` as [TweenSpec][androidx.compose.animation.core.TweenSpec].
+ *
+ * Usage:
+ * ```
+ * slideIn(
+ *     duration = 3.seconds,
+ *     delay = 1.seconds,
+ *     easing = LinearEasing,
+ *     initialOffset = { IntOffset(x = it.width, y = 0) }
+ * )
+ * ```
+ *
+ * ```
+ *   ^ x-coordinate
+ *   |
+ * w +   *   *
+ *   |
+ *   +           *
+ *   |
+ *   +               *
+ *   |
+ * 0 +                   *
+ *   |
+ *   + - + - + - + - + - + - > seconds
+ *       0   1   2   3   4
+ * ```
+ *
+ * @param duration duration of the animation spec
+ * @param delay the amount of time that animation waits before starting
+ * @param easing the easing curve that will be used to interpolate between start and end
+ * @param initialOffset a lambda that takes the full size of the content and returns the initial offset for the slide-in
+ * @author [Stanley Wintergreen](https://github.com/kepocnhh)
+ * @since 0.0.1
+ */
 fun slideIn(
     duration: Duration,
     delay: Duration,
@@ -26,6 +61,41 @@ fun slideIn(
     )
 }
 
+/**
+ * The wrapper for the [slideOut][androidx.compose.animation.slideOut] with an `animationSpec` as [TweenSpec][androidx.compose.animation.core.TweenSpec].
+ *
+ * Usage:
+ * ```
+ * slideOut(
+ *     duration = 3.seconds,
+ *     delay = 1.seconds,
+ *     easing = LinearEasing,
+ *     targetOffset = { IntOffset(x = it.width, y = 0) }
+ * )
+ * ```
+ *
+ * ```
+ *   ^ x-coordinate
+ *   |
+ * w +                   *
+ *   |
+ *   +               *
+ *   |
+ *   +           *
+ *   |
+ * 0 +   *   *
+ *   |
+ *   + - + - + - + - + - + - > seconds
+ *       0   1   2   3   4
+ * ```
+ *
+ * @param duration duration of the animation spec
+ * @param delay the amount of time that animation waits before starting
+ * @param easing the easing curve that will be used to interpolate between start and end
+ * @param targetOffset a lambda that takes the full size of the content and returns the target offset for the slide-out
+ * @author [Stanley Wintergreen](https://github.com/kepocnhh)
+ * @since 0.0.1
+ */
 fun slideOut(
     duration: Duration,
     delay: Duration,
